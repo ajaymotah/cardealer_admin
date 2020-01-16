3fdd84b63471f5a9af0c2492e572f3001efbbee0
Ajay <ajaymotah@gmail.com>
2020-01-16 06:04:50 +0400

    login/register form

--- pages/cd_register.php
+++ pages/cd_register.php
@@ -69,20 +69,7 @@
           <!-- /.col -->
         </div>
       </form>
-
-      <div class="social-auth-links text-center">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i>
-          Sign up using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i>
-          Sign up using Google+
-        </a>
-      </div>
-
-      <a href="examples/login.html" class="text-center">I already have a membership</a>
+    <a href="examples/login.html" class="text-center">I already have a membership</a>
     </div>
     <!-- /.form-box -->
   </div><!-- /.card -->
