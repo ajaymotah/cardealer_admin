22e2d9456fbf84f8f7cc0ede606bf527bfaa1305
Ajay <ajaymotah@gmail.com>
2020-04-18 11:37:57 +0400

    add/active users

---
+++ facebook_config/testpostpage.php
@@ -0,0 +1,42 @@
+<?php
+include('fb_init.php');
+include('../includes/db_operations.class.php');
+$listing_id=$_POST['listing_id'];
+
+//$listing_id=468;
+$car_preview=$db_operation->get_car_preview($listing_id);
+if($car_preview['sale_price']>100){
+  $car_price=number_format($car_preview['sale_price']);
+}
+else{
+  $car_price="Price is Negotiable";
+}
+/*
+$linkData = [
+ 'link' => 'www.yoururl.com',
+ 'message' => 'Your message here'
+];
+*/
+
+
+/*Token expired by March 2020*/
+$pageAccessToken ='EAAExKKM6XLoBAIQK7QLlIZATCUcAltaZBbTfHNsPGBnPo3FaIEyUbkDtTJKcsybfV5xvqTz2FrXhXLZAi40GQWAh65SVMbobZCpcZBy4wBWjc6KzPHG3Cxm2eU41jgVhAU9yYZBWsD3oFwaR3U7csVi0lr3Yi6r4xcqfpVXoWvnQZDZD';
+$imageData=[
+  'source'=>$fb->fileToUpload('http://cardealer.webdevsolutions.biz/admin/uploaded_images/'.$car_preview['listing_image_url']),
+  'message'=>'Testing!!!'
+
+];
+try {
+  //for feeds only
+ // $response = $fb->post('/814270918760767/feed', $linkData, $pageAccessToken);
+//-for photos--//
+ $response = $fb->post('/me/photos', $imageData, $pageAccessToken);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ echo 'Graph returned an error: '.$e->getMessage();
+ exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+ echo 'Facebook SDK returned an error: '.$e->getMessage();
+ exit;
+}
+$graphNode = $response->getGraphNode();
+ ?>
